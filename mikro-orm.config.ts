3209af2e5e9a3c36d05318d175857654f273be2c
Pedro Campagnoli <littleozuka@gmail.com>
2026-08-29 12:08:51 -0300

    chore: configure MikroORM

---
+++ mikro-orm.config.ts
@@ -0,0 +1,12 @@
+import { Migrator } from '@mikro-orm/migrations';
+import { defineConfig } from '@mikro-orm/postgresql';
+
+export default defineConfig({
+  host: process.env.POSTGRES_HOST ?? 'localhost',
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
+  user: process.env.POSTGRES_USER ?? 'postgres',
+  password: process.env.POSTGRES_PASSWORD ?? 'postgres',
+  dbName: process.env.POSTGRES_DB ?? 'wagering',
+
+  extensions: [Migrator],
+});
